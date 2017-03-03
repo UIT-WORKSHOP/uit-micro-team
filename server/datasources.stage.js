9f517734e88ea366d54668fd06afeb188fc4ba54
Panda Poe <me@pandapoe.com>
2017-03-03 19:32:42 +0700

    :wrench: Update data source config for stage/prod, mongodb

---
+++ server/datasources.stage.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "mongo": {
+    "host": "127.0.0.1",
+    "port": process.env.MDB_PORT || 27017,
+    "database": process.env.MDB_DATABASE || "uit-micro-team",
+    "password": process.env.MDB_PASSWORD || "123",
+    "name": "mongo",
+    "user": process.env.MDB_USER || "tan",
+    "connector": "mongodb"
+  }
+}
