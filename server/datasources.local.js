a28104d24e7ceec17d16a2fa0d33fce342c1080d
Panda Poe <me@pandapoe.com>
2017-03-03 20:02:11 +0700

    :wrench: replace default datasource wit local datasource

---
+++ server/datasources.local.js
@@ -0,0 +1,15 @@
+{
+  "db": {
+    "name": "db",
+    "connector": "memory"
+  },
+  "mongo": {
+    "host": "ds113680.mlab.com",
+    "port": 13680,
+    "database": "heroku_12xgjq6v",
+    "password": "123z",
+    "name": "mongo",
+    "user": "pandapoe",
+    "connector": "mongodb"
+  }
+}
