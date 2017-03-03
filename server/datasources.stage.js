b8148cb89603a7ffc5076b2a1ed13e308366a980
Panda Poe <me@pandapoe.com>
2017-03-03 19:38:15 +0700

    :wrench: Add config for MongoDB Host

--- server/datasources.stage.js
+++ server/datasources.stage.js
@@ -1,6 +1,6 @@
 module.exports = {
   "mongo": {
-    "host": "127.0.0.1",
+    "host": process.env.MDB_HOST || "127.0.0.1",
     "port": process.env.MDB_PORT || 27017,
     "database": process.env.MDB_DATABASE || "uit-micro-team",
     "password": process.env.MDB_PASSWORD || "123",
